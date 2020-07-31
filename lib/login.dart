3b4e6b5715492476550e14c1000ebb18874f06c4
sabanansari <alisaban52@gmail.com>
2020-07-31 06:21:39 +0530

    dialog alert created

--- lib/login.dart
+++ lib/login.dart
@@ -15,6 +15,7 @@ class LoginAndRegister extends StatefulWidget {
 enum FormType { login, register }
 
 class _LoginAndRegisterState extends State<LoginAndRegister> {
+  DialogBox dialogBox = DialogBox();
   var formKey = new GlobalKey<FormState>();
   FormType _formType = FormType.login;
   String _email = "";
@@ -49,10 +50,15 @@ class _LoginAndRegisterState extends State<LoginAndRegister> {
       try {
         if (_formType == FormType.login) {
           String userId = await widget.auth.SignIn(_email, _password);
+          dialogBox.information(context, "", "Logged In Successfully");
         } else {
           String userId = await widget.auth.SignUp(_email, _password);
+          dialogBox.information(context, "Congratulations!",
+              "Your account has been successfully created");
         }
+        widget.onSignedIn();
       } catch (e) {
+        dialogBox.information(context, "Error:", e.toString());
         print(e);
       }
     }
